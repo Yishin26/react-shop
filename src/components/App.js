2243589c349262efb0f6f87e0758e3019febdfd0
Louise <yishinlin@outlook.com>
2021-03-15 22:04:53 +0800

    基本傳參、單個組件架構

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import React from 'react'
 import Header from 'components/Header'
 import Products from 'components/Products'
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    
+    render() {
+        return (
             <div className='main'>
-               <Header/>
-               <Products/>
+                <Header nickname='ADMIN' age={28} marry={true} />
+                <Products />
 
             </div>
         )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
+import React from "react";
 
-import React from 'react'
-
-class Header extends React.Component{
-    render(){
-        return(
-            <div className='header'>
-               <div className="grid">
-                   <div className="start"><a href="/">HOME</a></div>
-                   <div className="end">
-                       <a href="/">Login</a>
-                       <a href="/">Register</a>
-                   </div>
-               </div>
+const Header = (props) => {
+    return (
+        <div className='header'>
+            <div className="header">
+                <div className="grid">
+                    <div className="start">
+                        <a href="/">HOME</a>
+                    </div>
+                    <div className="end">
+                        {props.nickname ? (
+                            <span className='nickname'>
+                                <i className='far fa-user'></i>
+                                {props.nickname}</span>
+                        ) : (
+                                <React.Fragment>
+                                    <a href="/">Login</a>
+                                    <a href="/">Register</a>
+                                </React.Fragment>
+                            )
+                        }
+                    </div>
+                </div>
             </div>
-        )
-    }
+        </div>
+
+    );
 }
+
+
+
 
 export default Header;
