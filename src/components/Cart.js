eb6eec4691de4a2e61d3fecceed33d009c29603f
Louise <yishinlin@outlook.com>
2021-04-07 21:16:04 +0800

    購物車頁面基本架構

---
+++ src/components/Cart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from 'Layout';
+import CartItem from './CartItem';
+
+const Cart = () => {
+    <Layout>
+        <div className="cart-page">
+            <span className="cart-title">Shopping Cart</span>
+            <div className="cart-list">
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cart-total">
+                Total:
+<span className="total-price">￥2345</span>
+            </div>
+        </div>
+    </Layout>
+}
+
+export default Cart
---
+++ src/components/dist/Cart.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Layout = _interopRequireDefault(require("Layout"));
+
+var _CartItem = _interopRequireDefault(require("./CartItem"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
